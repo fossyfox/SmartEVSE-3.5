bb5dc6a40f61cb21917982cbd98560a6da52910c
fossyfox <fossyfox>
2026-06-02 13:55:10 +0200

    add raw view back

--- SmartEVSE-3/app/src/components/AppSidebar.vue
+++ SmartEVSE-3/app/src/components/AppSidebar.vue
@@ -118,6 +118,16 @@ const groups = computed<NavGroup[]>(() => [
       },
     ],
   },
+  {
+    heading: 'Developer',
+    items: [
+      {
+        name: 'raw',
+        label: 'Raw Data',
+        icon: '<polyline points="16 18 22 12 16 6"/><polyline points="8 6 2 12 8 18"/>',
+      },
+    ],
+  },
 ])
 
 function closeOnMobile() {
